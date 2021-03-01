dd980917c6b6080422b3aed9e8b8ec7128aa85c0
Shaviyo Marasinghe <shaviyobm@outlook.com>
2021-03-01 12:24:51 -0500

    today's progress

--- snowBlowerRemoteAPI.py
+++ snowBlowerRemoteAPI.py
@@ -28,40 +28,53 @@ sim.simxFinish(-1) # just in case, close all opened connections
 clientID=sim.simxStart('127.0.0.1',19999,True,True,5000,5) # Connect to CoppeliaSim
 if clientID!=-1:
     print ('Connected to remote API server')
-    # Initializing the Sensors
-    [leftSensorReturnCode, leftHandle] = sim.simxGetObjectHandle(clientID, "VisionSensorLeft", sim.simx_opmode_blocking)
-    [centerSensorReturnCode, centerHandle] = sim.simxGetObjectHandle(clientID, "VisionSensorCenter", sim.simx_opmode_blocking)
-    [rightSensorReturnCode, rightHandle] = sim.simxGetObjectHandle(clientID, "VisionSensorRight", sim.simx_opmode_blocking)
+    
+    
+    # Initializing the 
+    
+    # [leftSensorReturnCode, leftHandle] = sim.simxGetObjectHandle(clientID, "VisionSensorLeft", sim.simx_opmode_blocking)
+    # [centerSensorReturnCode, centerHandle] = sim.simxGetObjectHandle(clientID, "VisionSensorCenter", sim.simx_opmode_blocking)
+    # [rightSensorReturnCode, rightHandle] = sim.simxGetObjectHandle(clientID, "VisionSensorRight", sim.simx_opmode_blocking)
     # sensors are then saved in the following tuple
-    visionSensor = (leftHandle, centerHandle, rightHandle)
+    # visionSensor = (leftHandle, centerHandle, rightHandle)
 
     # Initialization of the Motor joints
     [leftMotorReturnCode, leftJoint] = sim.simxGetObjectHandle(clientID, "LeftMotor", sim.simx_opmode_blocking)
     [rightMotorReturnCode, rightJoint] = sim.simxGetObjectHandle(clientID, "RightMotor", sim.simx_opmode_blocking)
-
-    NOMINAL_VELOCITY = -1 # Nominal velocisty that will be used
+    [chuteMotorReturnCode, chuteJoint] = sim.simxGetObjectHandle(clientID, "ChuteMotor", sim.simx_opmode_blocking)
+    print(leftMotorReturnCode, rightMotorReturnCode, chuteMotorReturnCode)
+    NOMINAL_VELOCITY = 1 # Nominal velocisty that will be used
     VAR = 1
 
     while True:
-        # Let's get this party Started
-        defaultReading = (False, False, False)
-        sensorReading = list(defaultReading)
-
-        for i in range(0, 3):
-            returnBool, state, aux = sim.simxReadVisionSensor(clientID, visionSensor[i], sim.simx_opmode_blocking)
-            if state > -1:
-                sensorReading[i] = aux[0][11] < 0.3
-
         rightV = NOMINAL_VELOCITY
         leftV = NOMINAL_VELOCITY
-
-        if sensorReading[0]:
-            leftV = -0.03
-        if sensorReading[2]:
-            rightV = -0.03
+        chuteV = NOMINAL_VELOCITY
 
         leftSensorReturnCode = sim.simxSetJointTargetVelocity(clientID, leftJoint, leftV, sim.simx_opmode_blocking)
         rightSensorReturnCode = sim.simxSetJointTargetVelocity(clientID, rightJoint, rightV, sim.simx_opmode_blocking)
+        chuteMotorReturnCode = sim.simxSetJointTargetVelocity(clientID, chuteJoint, chuteV, sim.simx_opmode_blocking)
+
+    # while True:
+    #     # Let's get this party Started
+    #     defaultReading = (False, False, False)
+    #     sensorReading = list(defaultReading)
+
+    #     for i in range(0, 3):
+    #         returnBool, state, aux = sim.simxReadVisionSensor(clientID, visionSensor[i], sim.simx_opmode_blocking)
+    #         if state > -1:
+    #             sensorReading[i] = aux[0][11] < 0.3
+
+    #     rightV = NOMINAL_VELOCITY
+    #     leftV = NOMINAL_VELOCITY
+
+    #     if sensorReading[0]:
+    #         leftV = -0.03
+    #     if sensorReading[2]:
+    #         rightV = -0.03
+
+    #     leftSensorReturnCode = sim.simxSetJointTargetVelocity(clientID, leftJoint, leftV, sim.simx_opmode_blocking)
+    #     rightSensorReturnCode = sim.simxSetJointTargetVelocity(clientID, rightJoint, rightV, sim.simx_opmode_blocking)
 
 
 
